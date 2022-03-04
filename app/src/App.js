85a703db683ae55adb85ac4c0e00e9080062b46a
Jeff Hughes <jeff.hughes@gmail.com>
2022-03-03 19:57:34 -0500

    Add fields for probability and quantity

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,34 @@
+import React from 'react';
 import "./App.css";
 import InventoryTable from './inventory.js';
 
 const armor = require('./data/armor.json');
 
-function App() {
-    return (
-        <div className="app">
-            <h2>D&amp;D 5th Edition Merchant Inventory Generator</h2>
-            <InventoryTable data={armor} />
-        </div>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {num: this.generateNumber()};
+        this.handleButtonClick = this.handleButtonClick.bind(this);
+    }
+
+    generateNumber() {
+        return Math.random();
+    }
+
+    handleButtonClick() {
+        this.setState({num: this.generateNumber()});
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <h2>D&amp;D 5th Edition Merchant Inventory Generator</h2>
+                <button type="button" onClick={this.handleButtonClick}>Button!</button>
+                <div>{this.state.num.toString()}</div>
+                <InventoryTable data={armor} />
+            </div>
+        );
+    }
 }
 
 export default App;
--- app/src/inventory.js
+++ app/src/inventory.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function InventoryTable(props) {
     let data = props.data;
 
@@ -5,7 +7,7 @@ function InventoryTable(props) {
         if ("items" in item) {
             return <InventoryCategory data={item} key={index} />;
         } else {
-            return <InventoryRow data={item} key={index} />;
+            return <tbody><InventoryRow data={item} key={index} /></tbody>;
         }
     });
 
@@ -13,10 +15,15 @@ function InventoryTable(props) {
         <div className="inventory_container">
             <h3>{data.category}</h3>
             <table className="inventory_table">
-                <tr>
-                    <th>Item</th>
-                    <th>Cost</th>
-                </tr>
+                <thead>
+                    <tr>
+                        <th className="table_left_align">Item</th>
+                        <th className="table_right_align">Cost</th>
+                        <th className="table_right_align">Probability</th>
+                        <th className="table_right_align">Min. Qty</th>
+                        <th className="table_right_align">Max. Qty</th>
+                    </tr>
+                </thead>
                 {rows}
             </table>
         </div>
@@ -30,20 +37,59 @@ function InventoryCategory(props) {
     );
     return (
         <tbody>
-            <tr><td colSpan="2" className="inventory_category">{data.name}</td></tr>
+            <tr className="inventory_category"><td colSpan="5">{data.name}</td></tr>
             {subitems}
         </tbody>
     );
 }
 
-function InventoryRow(props) {
-    let data = props.data;
-    return (
-        <tr>
-            <td>{data.name}</td>
-            <td>{data.cost.value} {data.cost.type}</td>
-        </tr>
-    );
+class InventoryRow extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            probability: Math.random().toFixed(2),
+            min_qty: 1,
+            max_qty: 10
+        };
+
+        this.handleFloat = this.handleFloat.bind(this);
+        this.handleInt = this.handleInt.bind(this);
+    }
+
+    handleFloat(event) {
+        let str = event.target.value;
+        let flt = parseFloat(str);
+        // ensure value is valid number between 0 and 1
+        if (str === "" || (!isNaN(str) && !isNaN(flt) && flt >= 0.0 && flt <= 1.0)) {
+            this.setState({probability: event.target.value.trim()});
+        }
+    }
+
+    handleInt(event) {
+        let str = event.target.value;
+        let int = parseInt(str);
+        // ensure value is valid integer greater/equal to 0
+        if (str === "" || (!isNaN(str) && !isNaN(int) && int >= 0)) {
+            let state_val = event.target.getAttribute("data-field");
+            let state_obj = {}
+            state_obj[state_val] = event.target.value.trim();
+            this.setState(state_obj);
+        }
+    }
+
+    render() {
+        let data = this.props.data;
+        let money = data.cost.value.toLocaleString("en-US");
+        return (
+            <tr>
+                <td className="table_left_align">{data.name}</td>
+                <td className="table_right_align">{money} {data.cost.type}</td>
+                <td className="table_right_align"><input type="text" className="inventory_prob_field" value={this.state.probability} onChange={this.handleFloat} /></td>
+                <td className="table_right_align"><input type="text" className="inventory_min_qty_field" value={this.state.min_qty} data-field="min_qty" onChange={this.handleInt} /></td>
+                <td className="table_right_align"><input type="text" className="inventory_max_qty_field" value={this.state.max_qty} data-field="max_qty" onChange={this.handleInt} /></td>
+            </tr>
+        );
+    }
 }
 
 export default InventoryTable;
